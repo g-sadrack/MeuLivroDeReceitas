dbd13dd6f7c4582676629b3a5de7950f5cce0330
Gabriel Sadrack <gabrielscr2023@gmail.com>
2026-01-24 17:02:31 -0300

    Added an endpoint to register a new user

---
+++ src/Shared/MyRecipeBook.Communication/Response/ResponseRegisteredUserJson.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyRecipeBook.Communication.Response
+{
+    public class ResponseRegisteredUserJson
+    {
+        public String Nome { get; set; } = String.Empty;
+    }
+}
