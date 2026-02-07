b2420c7da5eed2bee7aafdff8c02a9a01d1bb692
Gabriel Sadrack <gabrielscr2023@gmail.com>
2026-02-07 11:57:32 -0300

    Created user entity

---
+++ src/Backend/MyRecipeBook.Domain/MyRecipeBook.Domain/Entities/EntityBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyRecipeBook.Domain.Entities
+{
+    public class EntityBase
+    {
+        public int Id { get; set; }
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+
+    }
+}
