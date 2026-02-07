b741ed78e6153471a57f75992bbb51ca34cbf9d8
Gabriel Sadrack <gabrielscr2023@gmail.com>
2026-02-07 11:59:50 -0300

    Implemented encrypted password logic

---
+++ src/Backend/MyRecipeBook.Application/Services/Cryptography/PasswordEncripter.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace MyRecipeBook.Application.Services.Cryptography
+{
+    public class PasswordEncripter
+    {
+        public static string Encrypt(string password)
+        {
+            var chaveAdicional = "MyRecipeBook2024!";
+
+            var newPassword = $"{password}{chaveAdicional}";
+
+            var bytes = Encoding.UTF8.GetBytes(newPassword);
+            var hashBytes = SHA512.HashData(bytes);
+            return StringBytes(hashBytes);
+        }
+
+        public static string StringBytes(byte[] bytes)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var b in bytes)
+            {
+                sb.Append(b.ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
